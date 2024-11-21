fc1931972ba560170914968c8be47bd0d3f2e5dc
Barla-Charishma <charishmakishna5@gmail.com>
2024-11-21 08:29:51 +0530

    Solution

---
+++ app.py
@@ -0,0 +1,58 @@
+from openai import OpenAI 
+from apikey import api_data 
+import os
+import speech_recognition as sr 
+import pyttsx3 
+import webbrowser
+Model='gpt-4o'
+client =OpenAI(api_key=api_data)
+
+def Reply(question):
+    completion = client.chat.completions.create(
+        model=Model,
+        messages = [
+            
+            {'role':"system",'content':"you are a helpful assistant"},
+            {'role':'user','content':question}
+        ],
+        max_tokens=200
+    )
+    answer = completion.choice[0].message.content 
+    return answer 
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[0].id)
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+speak("hello how are you?")
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening ......')
+        r.pause_threshold =1  
+        audio=r.listen(source)
+    try:
+        print('Recognizing....')
+        query = r.recognize_google(audio,language = 'en-in')
+        print("User Said:{} \n".format(query))
+    except Exception as e:
+        print("Say tgat again....")
+        return "None"
+    return query 
+if __name__=='__main__':
+    while True:
+        query = takeCommand().lower()
+        if query =="none":
+            continue 
+        ans=Reply(query)
+        print(ans)
+        speak(ans)
+        if "Open youtube" in query:
+            webbrowser.open('www.youtube.com')
+        if "Open Google" in query:
+            webbrowser.open('www.Google.com')
+        if "bye " in query:
+            break
+                
